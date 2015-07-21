9857bf5ead5513a6d76d5d3569168e3b8192c360
Lianne <lyharrison@sky.com>
2015-07-21 00:57:28 +0000

    added in login and sign up feature

--- app/actions.rb
+++ app/actions.rb
@@ -13,11 +13,7 @@ get '/songs/new' do
 end
 
 post '/songs' do
-  @song = Song.new(
-    song_title: params[:song_title],
-    author: params[:author],
-    url:  params[:url]
-  )
+  @song = Song.new(params[:song])
   if @song.save
     redirect '/songs'
   else
@@ -26,10 +22,40 @@ post '/songs' do
 end
 
 get '/songs/:id' do
-  @song = Song.find params[:id]
+  @song = Song.find(params[:id])
   erb :'songs/show'
 end
 
+get '/users/signup' do
+  erb :'users/signup'
+end
+
+post '/users/signup' do
+  @user = User.new(params[:user])
+  if @user.save
+    redirect '/songs'
+  else
+    erb :'users/signup'
+  end
+end
+
+get '/users/login' do
+  erb :'users/login'
+end
+
+post '/users/login' do
+  @user = User.find_by_username(params[:username])
+  if @user == nil 
+    @error = 'Username not found!'
+    return erb :'users/login'
+  end
+  if @user.password == params[:password]
+    redirect '/songs'
+  else
+    @error = 'Password incorrect!'
+    erb :'users/login'
+  end
+end
 
 
 
